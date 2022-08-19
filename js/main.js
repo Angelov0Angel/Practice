ba25dfe4240f3f543615b016486b13592276af2d
Angelov0Angel <angelov0angel@gmail.com>
2022-08-19 21:50:12 +0200

    made changes on modal and js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 $(document).ready(function () {
     $(".mycard").fadeOut();
-
-
+ 
     $(document).scroll(function () {
-      var y = $(this).scrollTop();
-      if (y < 100) {
-          $(".mycard").fadeOut(2000);
-      } else {
-          $(".mycard").fadeIn(2000);
-      }
+       var y = $(this).scrollTop();
+       if (y < 100) {
+          $(".mycard").fadeOut(3000);
+       } else {
+          $(".mycard").fadeIn(3000);
+       }
     });
-
+ 
     $(".mycard").click(function () {
-    $(".myModal").css("display", "block");
+     let modal = $(".myModal");
+     let imgSource = $(this)[0].children[0].src;
+     modal[0].children[0].src = imgSource;
+     modal.css("display", "block");
+     $(".fadeBackground").css("display", "block");
+    });
+ 
+    $(".closeBtn").click(function () {
+     $(".myModal").css("display", "none");
+     $(".fadeBackground").css("display", "none");
+    });
+ 
+    $(".fadeBackground").click(function () {
+     $(".myModal").css("display", "none");
+     $(".fadeBackground").css("display", "none");
     })
-});
+ });
